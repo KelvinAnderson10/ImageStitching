ca8ff280fd0921e6ac0f55d9829d20b0caa6bc82
KelvinAnderson10 <72595945+KelvinAnderson10@users.noreply.github.com>
2020-10-14 21:00:24 +0700

    Codes

---
+++ imageStitching.py
@@ -0,0 +1,142 @@
+# Import libraries needed (OpenCV to train the image, numpy to use array, and pyplot to visualize plotting)
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# Read the images that we want to stitch together to create a panorama
+img_right = cv2.imread('img_right.jpg')
+img_left = cv2.imread('img_left.jpg')
+
+
+# Display the left and right images
+# Color that is read by openCV is in reverse order, which is BGR
+plt.figure(figsize=(30,20))
+plt.subplot(1,2,1)
+plt.title("Left Image")
+plt.imshow(img_left)
+plt.subplot(1,2,2)
+plt.title("Right Image")
+plt.imshow(img_right)
+plt.tight_layout()
+
+
+# Function to fix the color from BGR to RGB
+def fixColor(image):
+    return(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+
+
+# Convert the images into grayscale format
+img1 = cv2.cvtColor(img_right, cv2.COLOR_BGR2GRAY)
+img2 = cv2.cvtColor( img_left , cv2.COLOR_BGR2GRAY)
+
+
+# Display the converted Grayscales and RGB images
+plt.figure(figsize=(30,20))
+
+# RGB of "Left Image"
+plt.subplot(2,2,1)
+plt.title("Left Image")
+plt.imshow(fixColor(img_left))
+
+# Grayscale of "Left Image"
+plt.subplot(2,2,2)
+plt.title("Grayscale of Left Image")
+plt.imshow(img2)
+
+# RGB of "Right Image"
+plt.subplot(2,2,3)
+plt.title("Right Image")
+plt.imshow(fixColor(img_right))
+
+# Grayscale of "Right Image"
+plt.subplot(2,2,4)
+plt.title("Grayscale of Right Image")
+plt.imshow(img1)
+plt.tight_layout()
+
+
+# Create an object for ORB, used for getting keypoints and descriptors of image
+orb = cv2.ORB_create()
+
+
+# Get the keypoint and feature descriptor from the "Right Image"
+kp1, des1 = orb.detectAndCompute(img1,None)
+
+
+# Get the keypoint and feature descriptor from the "Left Image"
+kp2, des2 = orb.detectAndCompute(img2,None)
+
+
+# Get the "Right image" for visualizing the keypoints
+img_right_kp = cv2.drawKeypoints(img_right, kp1, np.array([]), color=(0,0, 255))
+
+
+# Get the "Left image" for visualizing the keypoints
+img_left_kp = cv2.drawKeypoints(img_left, kp2,np.array([]),color=(255,0,0))
+
+
+# Visualizing the keypoints from Left & Right images
+plt.figure(figsize=(30,20))
+plt.subplot(1,2,1)
+plt.imshow(fixColor(img_left_kp ))
+
+plt.subplot(1,2,2)
+plt.imshow(fixColor(img_right_kp ))
+plt.tight_layout()
+
+
+# Matching the descriptor of first set with the second set using distance calculation
+bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+
+
+# Pass the 2 Images as input arguements
+matches = bf.match(des1,des2)
+
+
+# Sort the matches in the order of their distances
+matches = sorted(matches, key = lambda x:x.distance)
+
+
+# Assign color to be used to draw the matches , we use yellow here
+draw_params = dict(matchColor = (255,255,0), flags = 2)
+
+
+# Displaying the image with 30 keypoints matched between both of the images
+matched_features_image = cv2.drawMatches(fixColor(img_right), kp1, fixColor(img_left), kp2, matches[:30], None,**draw_params)
+plt.figure(figsize=(30,20))
+plt.imshow(matched_features_image)
+
+
+# Get the best matches between both of the images
+if len(matches) >= 4:
+    src = np.float32([ kp1[m.queryIdx].pt for m in matches ]).reshape(-1,1,2)
+    dst = np.float32([ kp2[m.trainIdx].pt for m in matches ]).reshape(-1,1,2)
+
+    H, masked = cv2.findHomography(src, dst, cv2.RANSAC, 5.0)
+else:
+    raise AssertionError("Can't find enough keypoints.")
+
+
+# Apply perspective transformation of "Right image" (Homography matrix, size)
+dst = cv2.warpPerspective(img_right,H,(img_left.shape[1] + img_right.shape[1], img_left.shape[0]))
+
+
+# Visualize the output from the transformation
+plt.figure(figsize=(30,20))
+plt.title('Warped Image') 
+plt.imshow(fixColor(dst))
+
+
+#  Using left- image width and right- image width in the "Left Image"
+dst[0:img_left.shape[0], 0:img_left.shape[1]] = img_left
+
+
+# Store the resultant stitched Image
+cv2.imwrite('resultant_stitched_panorama.jpg',dst)
+
+
+# Display the stitched image of the Left and Right images
+plt.figure(figsize=(30,20))
+plt.title('Stitched Image')
+plt.imshow(fixColor(dst))
